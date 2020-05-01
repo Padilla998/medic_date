566170a5f269c2f25cf6dba36a154130f27832d8
Diego Padilla <fpadillacaballero@gmail.com>
2020-05-01 16:42:45 -0600

    NUEVA CONEXION

--- bdd.php
+++ bdd.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=calendar;charset=utf8', 'root', 'usbw');
+	$bdd = new PDO('mysql:host=35.222.8.186;dbname=calendar;charset=utf8', 'root', '132546');
 }
 catch(Exception $e)
 {
--- cn.php
+++ cn.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect("localhost","root","usbw","medicdate");
+$conexion = mysqli_connect("35.222.8.186","root","132546","medicdate");
 //if (!$conexion){
  //   echo ' ERROR AL CONECTAR A LA BASE DE DATOS';
 //}else{
