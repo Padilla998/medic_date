70f64118d68863bc4bb8d55e0cb6482beb82a239
Diego Padilla <fpadillacaballero@gmail.com>
2020-05-28 22:20:46 -0600

    FARMACIA

---
+++ upload/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8080/farmacia/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8080/farmacia/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/Users/fpadi/Desktop/SERVER/root/farmacia/upload/catalog/');
+define('DIR_SYSTEM', 'C:/Users/fpadi/Desktop/SERVER/root/farmacia/upload/system/');
+define('DIR_IMAGE', 'C:/Users/fpadi/Desktop/SERVER/root/farmacia/upload/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'usbw');
+define('DB_DATABASE', 'farmacia');
+define('DB_PORT', '3307');
+define('DB_PREFIX', 'md_');
---
+++ upload/system/storage/cache/56/567c8d0b5d137eaef46d89f7ef4566a28ed08d06dab34ed157029b3f67edbf26.php
@@ -0,0 +1,56 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_6face9b79eeba6be048aecdde8948a10f2fb5fdcdfab8c3df1bb0da7cc011c90 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer>
+  <div class=\"container\">
+    <a href=\"https://www.opencart.com\" target=\"_blank\">";
+        // line 3
+        echo (isset($context["text_project"]) ? $context["text_project"] : null);
+        echo "</a>|<a href=\"http://docs.opencart.com/en-gb/introduction/\" target=\"_blank\">";
+        echo (isset($context["text_documentation"]) ? $context["text_documentation"] : null);
+        echo "</a>|<a href=\"https://forum.opencart.com\" target=\"_blank\">";
+        echo (isset($context["text_support"]) ? $context["text_support"] : null);
+        echo "</a><br />
+  </div>
+</footer>
+</body></html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  23 => 3,  19 => 1,);
+    }
+}
+/* <footer>*/
+/*   <div class="container">*/
+/*     <a href="https://www.opencart.com" target="_blank">{{ text_project }}</a>|<a href="http://docs.opencart.com/en-gb/introduction/" target="_blank">{{ text_documentation }}</a>|<a href="https://forum.opencart.com" target="_blank">{{ text_support }}</a><br />*/
+/*   </div>*/
+/* </footer>*/
+/* </body></html>*/
+/* */
